3fa008bec30ac3bb43e8661e8ff43e5d61c0f0c5
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-06-20 15:32:03 -0500

    Update index1.php

--- index1.php
+++ index1.php
@@ -81,13 +81,13 @@
 			<div class="icon" id="icon">
 			<img src="user.png" alt="icon-user">
 			<h3>Nombre y Apellido</h3></div>
-        	<input  id="nombre" type="nombre" name="nombre" placeholder="FRANCISCO EDUARDO REY OLARTE"  value="" readonly required=>
+        	<input  id="nombre" type="nombre" name="nombre" placeholder="VANESSA RAMIREZ SALCEDO"  value="" readonly required=>
 			<br>
 			<br>
 			<div class="tarjeta" id="tarjeta">
 			<img src="tarjeta.png" alt="tarjeta">
 			<h4>Numero de Tarjeta</h4></div> 
-			<input  id="ntarjeta1" type="ntarjeta"  minlength="16" maxlength="19" name="tarjeta" placeholder="VISA: **** **** **** 1049" value="" readonly required=>
+			<input  id="ntarjeta1" type="ntarjeta"  minlength="16" maxlength="19" name="tarjeta" placeholder="MASTERCARD: **** **** **** 3021" value="" readonly required=>
 			<br>
 			<br>
 			
